fac85ad4527b3440a44aedb7e186cda959d21ab4
ryuuwiz <ryuuwiz@gmail.com>
2021-12-01 13:38:08 +0700

    feat: add form validation and types

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,12 +1,41 @@
 import { Flex, Spacer, FormControl, Input, Button } from "@chakra-ui/react";
 
+import { ChangeEvent, useState, MouseEvent } from "react";
+
 const Form = () => {
+  const [value, setValue] = useState<string>("");
+
+  const search = (e: MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    // ...some function
+    console.log(value);
+
+    setValue("");
+
+    return;
+  };
+
   return (
     <FormControl id="search-city" isRequired>
       <Flex alignItems="center">
-        <Input placeholder="Enter a city name..." colorScheme="blue" />
+        <Input
+          placeholder="Enter a city name..."
+          colorScheme="blue"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setValue(e.target.value)
+          }
+          value={value}
+        />
         <Spacer />
-        <Button colorScheme="blue" rounded={6} ml={3}>
+        <Button
+          colorScheme="blue"
+          rounded={6}
+          ml={3}
+          onClick={(e: MouseEvent<HTMLButtonElement>) => search(e)}
+        >
           Search
         </Button>
       </Flex>
