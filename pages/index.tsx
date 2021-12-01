944301da835aa851cfc9d38e11206744f091a203
ryuuwiz <ryuuwiz@gmail.com>
2021-12-01 14:34:31 +0700

    feat: add dummy text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import { Flex, Spacer } from "@chakra-ui/react";
+import { Flex, Stack, Container, Heading, Text } from "@chakra-ui/react";
 
 import Form from "../components/Form";
 
@@ -20,14 +20,45 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <Container>
         <Flex direction="column" background="gray.100" p={10} rounded={10}>
           <Form />
-          <Spacer />
+          <Stack spacing={6} py={10} px={2}>
+            <Heading size="lg" color="gray.600">
+              Depok
+            </Heading>
+            <Text
+              bgGradient="linear(to-l, #7928CA, #FF0080)"
+              bgClip="text"
+              fontSize="4xl"
+              fontWeight="extrabold"
+            >
+              29 °C
+            </Text>
+          </Stack>
+          <Stack spacing={2} p={2}>
+            <Text color="gray.500" fontSize="xl" fontWeight="medium">
+              Few clouds
+            </Text>
+            <Text color="gray.500" fontSize="md" fontWeight="medium">
+              Humidity: 42%
+            </Text>
+            <Text color="gray.500" fontSize="md" fontWeight="medium">
+              Wind Speed: 2.57 m/s
+            </Text>
+          </Stack>
         </Flex>
-      </main>
 
-      <footer></footer>
+        <Text
+          color="gray.100"
+          fontSize="md"
+          fontWeight="medium"
+          mt={10}
+          align="center"
+        >
+          © 2021 Ryuuwiz
+        </Text>
+      </Container>
     </Flex>
   );
 };
