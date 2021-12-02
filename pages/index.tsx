c135680273020a3ba8f921e7b53ee828c75cd771
ryuuwiz <ryuuwiz@gmail.com>
2021-12-02 12:31:54 +0700

    feat: add ip geo location for weather current location

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { InferGetStaticPropsType, NextPage, GetStaticProps } from "next";
+import type { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 import Head from "next/head";
 
-import { Flex, Container } from "@chakra-ui/react";
+import { Flex, Container, Alert, AlertIcon } from "@chakra-ui/react";
 
 import Form from "../components/Form";
 import Weather from "../components/Weather";
@@ -13,10 +13,22 @@ import axios from "axios";
 import useSWR from "swr";
 
 const fetcher = (url: any) => axios.get(url).then((res) => res.data);
-const defaultCity: string = "depok";
 
-const Home: NextPage = () => {
-  const [city, setCity] = useState<string>(defaultCity);
+export const getStaticProps: GetStaticProps = async () => {
+  const { region } = await fetcher("https://json.geoiplookup.io/");
+
+  return {
+    props: {
+      region,
+    },
+    revalidate: 5,
+  };
+};
+
+const Home: NextPage = ({
+  region,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [city, setCity] = useState<string>(() => (region ? region : ""));
   const { data, error } = useSWR(
     () => (city ? `/api/weather/${city}` : null),
     fetcher
@@ -28,27 +40,38 @@ const Home: NextPage = () => {
   };
 
   return (
-    <Flex
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      bgGradient="linear(to-l, #7928CA, #FF0080)"
-      w="100vw"
-    >
-      <Head>
-        <title>Weather App</title>
-        <meta name="description" content="Weather app using nextjs" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Flex
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        w="100vw"
+      >
+        <Head>
+          <title>Weather App</title>
+          <meta name="description" content="Weather app using nextjs" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Container>
-        <Flex direction="column" background="gray.100" p={10} rounded={10}>
-          <Form getWeather={getWeather} />
-          {!data && !error ? <SkeletonWeather /> : <Weather data={data} />}
-        </Flex>
-        <Footer />
-      </Container>
-    </Flex>
+        <Container>
+          <Flex direction="column" background="gray.100" p={10} rounded={10}>
+            <Form getWeather={getWeather} />
+            {!data && !error ? (
+              <SkeletonWeather />
+            ) : !data ? (
+              <Alert status="error" my={5} p={5} rounded={10}>
+                <AlertIcon />
+                Not Found !
+              </Alert>
+            ) : (
+              <Weather data={data} />
+            )}
+          </Flex>
+          <Footer />
+        </Container>
+      </Flex>
+    </>
   );
 };
 
